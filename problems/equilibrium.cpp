7947bebd73289f8e276ebb3d94cc94749f97aa1a
Simranpal Singh <navigill20@gmail.com>
2015-11-02 00:11:26 -0800

    problems\equilibrium.cpp
    Added the equilibrium problem statement

--- problems/equilibrium.cpp
+++ problems/equilibrium.cpp
@@ -1,3 +1,17 @@
+/*Problem : Find an i, such that, A1+A2...Ai-1 =Ai+1+Ai+2...An.
+
+Input Format 
+The first line contains T, the number of test cases. For each test case, the first line contains N, the number of elements in the array A. The second line for each test case contains N space-separated integers, denoting the array A.
+
+Output Format 
+For each test case print YES if there exists an element in the array, such that the sum of the elements on its left is equal to the sum of the elements on its right; otherwise print NO.
+
+Constraints 
+1≤T≤10 
+1≤N≤105 
+1≤Ai ≤2×104 
+1≤i≤N
+*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
